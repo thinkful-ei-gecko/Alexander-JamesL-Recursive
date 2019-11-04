8fb12b7bc756c5a55ec9a809f2bef2783b3c30d4
Alexander Fukui <alexander.fukui@gmail.com>
2019-11-04 16:09:58 -0600

    fibonacci kinda solved

--- index.js
+++ index.js
@@ -66,3 +66,16 @@ function stringSplitter(string, splitter) {
 
 
 console.log(stringSplitter('02/20/2020', '/'))
+
+//#6 fibonacci
+
+function fibonacci(n) {
+  if (n <= 0)
+  return 0;
+  if (n <= 2)
+  return 1;
+  
+  return fibonacci(n-1) + fibonacci(n-2)
+  }
+  
+console.log(fibonacci(7))
