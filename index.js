b491db9aa1d857b5f8450afe2f9614a171a99ee0
James Lee <jameswlee109@gmail.com>
2019-11-04 11:27:31 -0600

    first two drills complete

---
+++ index.js
@@ -0,0 +1,26 @@
+//#1 COUNTING SHEEP
+function countSheep(num) {
+  if(num === 0) {
+    return 'All sheep jumped over the fence';
+  }
+  else {
+    console.log(num + ': Another sheep jumps over the fence')
+    countSheep(num - 1)
+  }
+}
+countSheep(3)
+
+//#2 Power Calculator
+function powerCalc(base, exp) {
+  if(exp < 0) {
+    return 'exp should be >= 0'
+  }
+  if(exp === 0) {
+    return 1;
+  }
+  else {
+    return base * powerCalc(base, exp - 1)
+  }
+}
+
+powerCalc(10, 2)
